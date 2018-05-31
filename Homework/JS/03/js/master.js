dbc359c6be76f263d72afc27924d518dc9abcbdd
grafundzijus <grafas81@gmail.com>
2018-05-31 12:18:10 +0300

    pirmas įkėlimas

---
+++ Homework/JS/03/js/master.js
@@ -0,0 +1,9 @@
+function amžius(){
+  if(metai >= 18){
+    alert("Sveiki atvykę!");
+  }
+  else{
+    alert("Deja, svetainėje negalite lankytis");
+  }
+}
+  
---
+++ Homework/JS/04/js/master.js
@@ -0,0 +1,5 @@
+function greetings(name, sname){
+  var name = document.getElementById('name').value;
+  var sname = document.getElementById('sname').value;
+  prompt(name + " " + sname + " kątik prisijungė");
+}
